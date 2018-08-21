c73c683195dbd9bce6c1215da818bb2ee57b2567
Yinaijing <jingjingyinai@163.com>
2018-08-21 14:30:54 +0800

    Merge branch 'master' of https://github.com/Yinaijing/hiworld
                        # Conflicts:
                        #	src/HiWorld.java

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -5,7 +5,6 @@ public class HiWorld {
         System.out.println("Hi World yinaijing");
         System.out.println("df");
         System.out.println("commit");
-        System.out.println("commit2");
         System.out.println("sd");
     }
 }
